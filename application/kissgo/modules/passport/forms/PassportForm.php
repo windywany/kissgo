3770050b2cb901a3655201d31f96e3c25c2c6926
Leo Ning <windywany@163.com>
2012-11-26 22:28:55 +0800

    为添加表单添加语言选择项

--- application/kissgo/modules/passport/forms/PassportForm.php
+++ application/kissgo/modules/passport/forms/PassportForm.php
@@ -10,4 +10,5 @@ class PassportForm extends TableForm {
     var $account = array('label' => '用户名', 'validator' => array('required' => '用户名不能为空.'));
     var $passwd = array('label' => '密码', 'validator' => array('required' => '密码不能为空', 'maxlength(15)' => '密码最大长度为15个字符.', 'minlength(6)' => '密码最小长度为6个字符.'));
     var $captcha = array('label' => '验证码');
+    var $lang = array('label' => 'Language');
 }
