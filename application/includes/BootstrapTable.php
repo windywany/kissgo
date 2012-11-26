3168cd9fd4472f056f56294bc40404a1bfa1d183
windywany <windywany@163.com>
2012-11-26 15:49:38 +0800

    refine the code of kissgo form, table and template

---
+++ application/includes/BootstrapTable.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * kissgo framework that keep it simple and stupid, go go go ~~
+ *
+ * @author Leo Ning
+ * @package kissgo.core
+ *
+ * $Id$
+ */
+/**
+ * 
+ * Bootstrap 样式表格
+ * @author Leo Ning
+ *
+ */
+class BootstrapTable extends SimpleTable {
+	public function __construct($data, $extraData = array(), $properties = array()) {
+		parent::__construct ( $data, $extraData, $properties );
+		$this->_properties ['class'] = 'table table-hover ' . $this->_properties ['class'];
+	}
+}
+
